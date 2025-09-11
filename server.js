db9fdf2382168362207ef92b8c2a688eaa696b80
andreapaccagnella94 <andreapacca94@live.it>
2025-09-11 21:03:21 +0200

    add I start the new assigned exercise with the correct repo

--- server.js
+++ server.js
@@ -1,46 +1,30 @@
 /*
 Esercizio
-Creare  le rotte necessario per l'entità post all'interno di server.js e creare le rotte per le operazioni CRUD (Index, Show, Create, Update e Delete)
+Milestone 1
 
-Tutte le risposte saranno dei testi che confermeranno l’operazione che il server deve eseguire, secondo le convenzioni REST.
+Come prima cosa, creiamo un controller per i nostri post, in una cartella controllers. 
 
-Ad esempio: 
+All’interno, prepariamo tutte le funzioni necessarie e copiamo in ciascuna la logica delle funzioni che attualmente si trovano nel router (al momento restituiscono solo dei messaggi). 
 
-Se viene chiamata /posts col verbo GET ci aspettiamo “Lista dei post”;
+Poi torniamo sul file delle rotte. Qui importiamo le funzioni dichiarate nel controller e le associamo alle varie rotte, come visto in classe.
 
-Se viene chiamato /posts/1 col verbo DELETE ci aspettiamo “Cancellazione del post 1”
+Testiamo su postman se chiamando gli endpoint riceviamo effettivamente le stesse risposte che avevamo prima. 
 
-e via dicendo…
+Se tutto funziona, passiamo alla prossima milestone
 
+Milestone 2
 
+Per iniziare, creiamo una cartella data  in cui creare un file che contenga ed esporti l’array di posts che trovate in allegato.  Importiamo questo file in cima al controller. 
 
-Nota:
+Ora passiamo ad implementare le logiche delle nostre CRUD:
 
-Avete anche l’array dei post che vi abbiamo fornito, salvatelo da qualche parte.
-Ci servirà per i prossimi step.
-Per oggi vi può servire in caso vogliate provare i bonus.
+Index dovrà restituire la lista dei post in formato JSON
+Show dovrà restituire un singolo post in formato JSON
+Destroy dovrà eliminare un singolo post dalla lista, stampare nel terminale (console.log) la lista aggiornata, e rispondere con uno stato 204 e nessun contenuto.
 
 Bonus
-Provare a restituire la lista dei post dalla rotta index, in formato json
-Provare a restituire un singolo post dalla rotta show, sempre in formato json
-*/
-// SECONDA CONSEGNA
-/*
-Creare un file di routing (routers/posts.js) che conterrà le rotte necessario per l'entità post.
-
-All'interno creare le rotte per le operazioni CRUD (Index, Show, Create, Update e Delete)
-
-Tutte le risposte saranno dei testi che confermeranno l’operazione che il server deve eseguire, secondo le convenzioni REST.
-
-Ad esempio: 
-
-Se viene chiamata /posts col verbo GET ci aspettiamo “Lista dei post”;
-
-Se viene chiamato /posts/1 col verbo DELETE ci aspettiamo “Cancellazione del post 1”
-
-e via dicendo…
-
-Registrare il router dentro app.js con il prefisso posts/.
+Implementare un filtro di ricerca nella index che mostri solo i post che hanno un determinato Tag
+In Show e Destroy, controllare se il parametro si riferisce ad un post esistente, in caso contrario, rispondere con uno stato 404 e un messaggio d’errore, sempre in formato JSON.
 */
 
 const express = require('express')
